e1d747a6477dc81b1a8aa9866ffe38e41b5134a4
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2022-03-10 11:11:57 +0200

    formatted-separated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,48 @@
 import React, { Component } from 'react';
 import PhonebookForm from 'components/PhonebookForm/PhonebookForm';
 import Filter from 'components/Filter/Filter';
-import PhonebookList from 'components/PhonebookList/PhonebookList'
+import PhonebookList from 'components/PhonebookList/PhonebookList';
 import shortid from 'shortid';
 
 class App extends Component {
   state = {
     contacts: [],
     filter: '',
-  }
+  };
 
-  componentDidMount() { 
+  componentDidMount() {
     const contacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(contacts);
 
-    if (parsedContacts) { 
-      this.setState({contacts:parsedContacts})
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
     }
-
   }
-  componentDidUpdate(prevProps,prevState) { 
-    const nextContact = this.state.contacts
+  componentDidUpdate(prevProps, prevState) {
+    const nextContact = this.state.contacts;
     const prevContact = prevState.contacts;
     if (nextContact !== prevContact) {
-      localStorage.setItem('contacts',JSON.stringify(nextContact))
+      localStorage.setItem('contacts', JSON.stringify(nextContact));
     }
   }
 
+  addNewContact = items => {
+    const { contacts } = this.state;
+    const searchContact = contacts
+      .map(contact => contact.name)
+      .includes(items.name);
 
-  addNewContact=(items)=>{
-    const {contacts}=this.state
-    const searchContact=contacts.map(contact=>contact.name).includes(items.name)
-
-    if(searchContact){
-      alert(`${items.name} is already in conacts`)
+    if (searchContact) {
+      alert(`${items.name} is already in conacts`);
+    } else {
+      this.setState({
+        contacts: [
+          ...this.state.contacts,
+          { id: shortid.generate(), ...items },
+        ],
+      });
     }
-    else {
-      this.setState({contacts:[...this.state.contacts,{id:shortid.generate(),...items}]})
-    }
-  }
+  };
 
   changeFilter = e => {
     this.setState({ filter: e.currentTarget.value });
@@ -46,33 +50,34 @@ class App extends Component {
 
   getVisibleContacts = () => {
     const { filter, contacts } = this.state;
-    
+
     return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase()),
+      contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-  deleteContact=(contactId)=>{
-    this.setState(prevState=>({
-      contacts:prevState.contacts.filter(contact=>contact.id!==contactId)
-    }))
-  }
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
 
-  render(){
-    const visibleContacts=this.getVisibleContacts()
+  render() {
+    const visibleContacts = this.getVisibleContacts();
 
-    return(
+    return (
       <div>
         <h1>Phonebook</h1>
-        <PhonebookForm onAddContact={this.addNewContact}/>
+        <PhonebookForm onAddContact={this.addNewContact} />
         <h2>Contacts</h2>
-        <Filter value={this.state.filter} onChange={this.changeFilter}/>
-        <PhonebookList visibleContacts={visibleContacts} deleteContact={this.deleteContact}/>
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
+        <PhonebookList
+          visibleContacts={visibleContacts}
+          deleteContact={this.deleteContact}
+        />
       </div>
-    )}
-  
+    );
+  }
 }
 
 export default App;
-
-
--- src/components/PhonebookList/PhonebookList.js
+++ src/components/PhonebookList/PhonebookList.js
@@ -1,14 +1,19 @@
-import React from "react";
-import s from './PhonebookList.module.css'
+import React from 'react';
+import PhonebookItem from '../PhonebookItem/PhonebookItem.js';
+import s from './PhonebookList.module.css';
 
-const PhonebookList=({visibleContacts,deleteContact})=>{
-    return(
-        <ul className={s.list}>
-        {visibleContacts.map(contact=>
-          <li className={s.contact} key={contact.id}>{contact.name}: {contact.number} <button type='button' className={s.delete} onClick={()=>deleteContact(contact.id)}>Delete</button></li>
-        )}
-      </ul>
-    )
-}
+const PhonebookList = ({ visibleContacts, deleteContact }) => {
+  return (
+    <ul className={s.list}>
+      {visibleContacts.map(contact => (
+        <PhonebookItem
+          key={contact.id}
+          contact={contact}
+          deleteContact={deleteContact}
+        />
+      ))}
+    </ul>
+  );
+};
 
-export default PhonebookList
+export default PhonebookList;
